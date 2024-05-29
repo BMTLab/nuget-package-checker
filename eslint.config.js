15b13b6e9f8a640795f82d12eeed40fa0a671a56
Nikita Neverov <neverovnikita.bmt@gmail.com>
2024-05-29 12:04:57 +0200

    Update to eslint@9

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,34 @@
 // eslint.config.js
+import js from '@eslint/js'
 import globals from 'globals'
 
-import path from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
-import pluginJs from '@eslint/js'
-
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended })
-
 export default [
-  { ignores: ['dist'] },
-  { languageOptions: { globals: globals.browser } },
-  ...compat.extends('standard')
+  js.configs.recommended,
+  { ignores: ['dist', 'coverage'] },
+  { languageOptions: { globals: globals.node, ecmaVersion: 'latest', sourceType: 'module' } },
+  {
+    rules: {
+      // Best Practices
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+
+      // Stylistic Issues
+      'brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
+      'comma-spacing': ['error', { 'before': false, 'after': true }],
+      'comma-dangle': ['error', 'never'],
+      'indent': ['error', 2, { 'SwitchCase': 1 }],
+      'key-spacing': ['error', { 'beforeColon': false, 'afterColon': true }],
+      'linebreak-style': ['error', 'unix'],
+      'quotes': ['error', 'single', { 'avoidEscape': true }],
+      'semi': ['error', 'never'],
+      'space-before-blocks': ['error', 'always'],
+      'space-in-parens': ['error', 'never'],
+      'space-infix-ops': 'error',
+      'keyword-spacing': ['error', { 'before': true, 'after': true }],
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 1 }],
+      'no-undef': 'off'
+    }
+  }
 ]
